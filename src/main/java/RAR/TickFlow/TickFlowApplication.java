1e93add00116492c9bb368fcc812d2a36e3bb7f5
ruzi <ruzi.x4nc4n0v@gmail.com>
2026-05-12 15:32:31 +0400

    chore: Project initialization

---
+++ src/main/java/RAR/TickFlow/TickFlowApplication.java
@@ -0,0 +1,13 @@
+package RAR.TickFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RAR/TickFlow/TickFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package RAR.TickFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
