d92c0c6beedf0589e39dbd1af712e5c55a83c9cd
ruzi <ruzi.x4nc4n0v@gmail.com>
2026-05-12 17:25:31 +0400

    feat: add entities and task tag enums

---
+++ src/main/java/RAR/TickFlow/enums/Priority.java
@@ -0,0 +1,7 @@
+package RAR.TickFlow.enums;
+
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ src/main/java/RAR/TickFlow/enums/Tag.java
@@ -0,0 +1,12 @@
+package RAR.TickFlow.enums;
+
+public enum Tag {
+    WORK,
+    STUDY,
+    PERSONAL,
+    HEALTH,
+    SHOPPING,
+    URGENT,
+    MEETING,
+    OTHER
+}
